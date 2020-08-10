0183d18393b65fae8b3ecbd79db7e57f0c4d2ecb
Allen Day <allenday@users.noreply.github.com>
2020-08-10 18:53:44 +0800

    Update getData.sql

--- getData.sql
+++ getData.sql
@@ -69,25 +69,114 @@ SELECT
     sums.hits
   ) AS session
   ,STRUCT(
-    "TODO" as todo
+    "TODO" as referralPath,
+    "TODO" as fullReferrer,
+    "TODO" as campaign,
+    "TODO" as source,
+    "TODO" as medium,
+    "TODO" as sourceMedium,
+    "TODO" as keyword,
+    "TODO" as adContent,
+    "TODO" as socialNetwork,
+    "TODO" as hasSocialSourceReferral,
+    "TODO" as campaignCode,
+    -- METRICS
+    "TODO" as organicSearches
   ) AS traffic_sources
   ,STRUCT(
-    "TODO" as todo
+    "TODO" as continent,
+    "TODO" as subContinent,
+    "TODO" as country,
+    "TODO" as region,
+    "TODO" as metro,
+    "TODO" as city,
+    "TODO" as latitude,
+    "TODO" as longitude,
+    "TODO" as networkDomain,
+    "TODO" as networkLocation,
+    "TODO" as cityId,
+    "TODO" as continentId,
+    "TODO" as countryIsoCode,
+    "TODO" as metroId,
+    "TODO" as regionId,
+    "TODO" as regionIsoCode,
+    "TODO" as subContinentCode
   ) AS geo_network
   ,STRUCT(
-    "TODO" as todo
+    "TODO" as userAgeBracket,
+    "TODO" as userGender,
+    "TODO" as interestOtherCategory,
+    "TODO" as interestAffinityCategory,
+    "TODO" as interestInMarketCategory
   ) AS audience
   ,STRUCT(
-    "TODO" as todo
+    -- METRICS
+    "TODO" as pageLoadTime,
+    "TODO" as pageLoadSample,
+    "TODO" as avgPageLoadTime,
+    "TODO" as domainLookupTime,
+    "TODO" as avgDomainLookupTime,
+    "TODO" as pageDownloadTime,
+    "TODO" as avgPageDownloadTime,
+    "TODO" as redirectionTime,
+    "TODO" as avgRedirectionTime,
+    "TODO" as serverConnectionTime,
+    "TODO" as avgServerConnectionTime,
+    "TODO" as serverResponseTime,
+    "TODO" as avgServerResponseTime,
+    "TODO" as speedMetricsSample,
+    "TODO" as domInteractiveTime,
+    "TODO" as avgDomInteractiveTime,
+    "TODO" as domContentLoadedTime,
+    "TODO" as avgDomContentLoadedTime,
+    "TODO" as domLatencyMetricsSample
   ) AS site_speed
   ,STRUCT(
-    "TODO" as todo
+    "TODO" as exceptionDescription,
+    -- METRICS
+    "TODO" as exceptions,
+    "TODO" as exceptionsPerScreenview,
+    "TODO" as fatalExceptions,
+    "TODO" as fatalExceptionsPerScreenview  
   ) AS exceptions
   ,STRUCT(
-    "TODO" as todo
+    "TODO" as date,
+    "TODO" as year,
+    "TODO" as month,
+    "TODO" as week,
+    "TODO" as day,
+    "TODO" as hour,
+    "TODO" as minute,
+    "TODO" as nthMonth,
+    "TODO" as nthWeek,
+    "TODO" as nthDay,
+    "TODO" as nthMinute,
+    "TODO" as dayOfWeek,
+    "TODO" as dayOfWeekName,
+    "TODO" as dateHour,
+    "TODO" as dateHourMinute,
+    "TODO" as yearMonth,
+    "TODO" as yearWeek,
+    "TODO" as isoWeek,
+    "TODO" as isoYear,
+    "TODO" as isoYearIsoWeek,
+    "TODO" as nthHour    
   ) AS time
   ,STRUCT(
-    "TODO" as todo
+    "TODO" as appInstallerId,
+    "TODO" as appVersion,
+    "TODO" as appName,
+    "TODO" as appId,
+    "TODO" as screenName,
+    "TODO" as screenDepth,
+    "TODO" as landingScreenName,
+    -- METRICS
+    "TODO" as exitScreenName,
+    "TODO" as screenviews,
+    "TODO" as uniqueScreenviews,
+    "TODO" as screenviewsPerSession,
+    "TODO" as timeOnScreen,
+    "TODO" as avgScreenviewDuration
   ) AS app_tracking
   ,STRUCT(
     "TODO" as pageTitle,
