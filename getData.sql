36679beecc78e1351af7efa555113e7fe890d16f
Allen Day <allenday@users.noreply.github.com>
2020-08-12 19:41:22 +0800

    Update getData.sql

--- getData.sql
+++ getData.sql
@@ -1,48 +1,56 @@
 WITH s0 AS (
   SELECT 
     session_id, 
-    COUNT(*) AS pages, 
-    MIN(block_timestamp) AS session_timestamp,
-    CAST(MIN(block_timestamp) AS DATE) AS d, 
-    MIN(block_timestamp) AS start_time, 
-    MAX(block_timestamp) AS end_time, 
-    UNIX_SECONDS(MAX(block_timestamp)) - UNIX_SECONDS(MIN(block_timestamp)) AS duration
-  FROM `crypto-public-data.aux.ethereum_sessions` AS s
-  GROUP BY session_id
+    transaction_count AS pages,
+    CAST(start_time AS DATE) AS date, 
+    start_time, end_time, duration
+  FROM
+    `crypto-public-data.aux.materialized_sessions` AS sessions
+  WHERE TRUE
+    AND EXISTS(SELECT 1 FROM UNNEST(sessions.to_address_set) AS x WHERE x = '0x32666b64e9fd0f44916e1378efb2cfa3b3b96e80') 
+--       '0x903dc47aa7c40f9f59ef1e5c167ce1a9b39a2bff' --origin protocol token
+--       '0x32666b64e9fd0f44916e1378efb2cfa3b3b96e80' -- renbridge
+    AND ARRAY_LENGTH(sessions.to_address_set) > 1 -- sessions involving more than one to_address
 )
+
 ,averages AS (
   SELECT
-    d, 
+    date, 
     AVG(pages) AS avg_session_pages,
     AVG(duration) AS avg_session_duration
   FROM s0
-  GROUP BY d
+  GROUP BY date
 )
 ,sums AS (
-  SELECT d,
+  SELECT date,
   SUM(pages) AS hits
   FROM s0
-  GROUP BY d
+  GROUP BY date
 )
 ,bounces AS (
   SELECT 
-    d, 
+    date, 
     COUNT(session_id) AS sessions
   FROM s0
   WHERE pages = 1
-  GROUP BY d
+  GROUP BY date
 )
 ,sessions AS (
   SELECT 
-    d, 
+    date, 
     COUNT(session_id) AS sessions
   FROM s0
   WHERE pages > 1
-  GROUP BY d
+  GROUP BY date
 )
 
+##########
+##########
+##########
+
 SELECT 
-  sessions.d AS date
+  sessions.date,
+  s0.session_id
   ,STRUCT(
     "TODO" as userType,
     "TODO" AS sessionCount,
@@ -51,11 +59,11 @@ SELECT
     "TODO" AS newUsers,
     -- METRICS
     "TODO" AS percentNewSessions,
-    "TODO" AS `1dayUsers`,
-    "TODO" AS `7dayUsers`,
-    "TODO" AS `14dayUsers`,
-    "TODO" AS `28dayUsers`,
-    "TODO" AS `30dayUsers`,
+    "TODO" AS _1dayUsers,
+    "TODO" AS _7dayUsers,
+    "TODO" AS _14dayUsers,
+    "TODO" AS _28dayUsers,
+    "TODO" AS _30dayUsers,
     "TODO" AS sessionsPerUser
   ) AS user
   ,STRUCT(
@@ -70,38 +78,38 @@ SELECT
     sums.hits
   ) AS session
   ,STRUCT(
-    "TODO" as referralPath,
-    "TODO" as fullReferrer,
-    "TODO" as campaign,
-    "TODO" as source,
-    "TODO" as medium,
-    "TODO" as sourceMedium,
-    "TODO" as keyword,
-    "TODO" as adContent,
-    "TODO" as socialNetwork,
-    "TODO" as hasSocialSourceReferral,
-    "TODO" as campaignCode,
-    -- METRICS
-    "TODO" as organicSearches
+--     "TODO" as referralPath,
+    "TODO" as fullReferrer
+--     "TODO" as campaign,
+--     "TODO" as source,
+--     "TODO" as medium,
+--     "TODO" as sourceMedium,
+--     "TODO" as keyword,
+--     "TODO" as adContent,
+--     "TODO" as socialNetwork,
+--     "TODO" as hasSocialSourceReferral,
+--     "TODO" as campaignCode,
+--     -- METRICS
+--     "TODO" as organicSearches
   ) AS traffic_sources
   ,STRUCT(
-    "TODO" as continent,
-    "TODO" as subContinent,
-    "TODO" as country,
-    "TODO" as region,
-    "TODO" as metro,
-    "TODO" as city,
-    "TODO" as latitude,
-    "TODO" as longitude,
-    "TODO" as networkDomain,
-    "TODO" as networkLocation,
-    "TODO" as cityId,
-    "TODO" as continentId,
-    "TODO" as countryIsoCode,
-    "TODO" as metroId,
-    "TODO" as regionId,
-    "TODO" as regionIsoCode,
-    "TODO" as subContinentCode
+--     "TODO" as continent,
+--     "TODO" as subContinent,
+--     "TODO" as country,
+--     "TODO" as region,
+--     "TODO" as metro,
+--     "TODO" as city,
+--     "TODO" as latitude,
+    "TODO" as longitude
+--     "TODO" as networkDomain,
+--     "TODO" as networkLocation,
+--     "TODO" as cityId,
+--     "TODO" as continentId,
+--     "TODO" as countryIsoCode,
+--     "TODO" as metroId,
+--     "TODO" as regionId,
+--     "TODO" as regionIsoCode,
+--     "TODO" as subContinentCode
   ) AS geo_network
   ,STRUCT(
     "TODO" as userAgeBracket,
@@ -113,103 +121,103 @@ SELECT
   ,STRUCT(
     -- METRICS
     "TODO" as pageLoadTime,
-    "TODO" as pageLoadSample,
-    "TODO" as avgPageLoadTime,
-    "TODO" as domainLookupTime,
-    "TODO" as avgDomainLookupTime,
-    "TODO" as pageDownloadTime,
-    "TODO" as avgPageDownloadTime,
-    "TODO" as redirectionTime,
-    "TODO" as avgRedirectionTime,
-    "TODO" as serverConnectionTime,
-    "TODO" as avgServerConnectionTime,
-    "TODO" as serverResponseTime,
-    "TODO" as avgServerResponseTime,
-    "TODO" as speedMetricsSample,
-    "TODO" as domInteractiveTime,
-    "TODO" as avgDomInteractiveTime,
-    "TODO" as domContentLoadedTime,
-    "TODO" as avgDomContentLoadedTime,
-    "TODO" as domLatencyMetricsSample
+--     "N/A" as pageLoadSample,
+    "TODO" as avgPageLoadTime
+--     "N/A" as domainLookupTime,
+--     "N/A" as avgDomainLookupTime,
+--     "N/A" as pageDownloadTime,
+--     "N/A" as avgPageDownloadTime,
+--     "N/A" as redirectionTime,
+--     "N/A" as avgRedirectionTime,
+--     "N/A" as serverConnectionTime,
+--     "N/A" as avgServerConnectionTime,
+--     "N/A" as serverResponseTime,
+--     "N/A" as avgServerResponseTime,
+--     "N/A" as speedMetricsSample,
+--     "N/A" as domInteractiveTime,
+--     "N/A" as avgDomInteractiveTime,
+--     "N/A" as domContentLoadedTime,
+--     "N/A" as avgDomContentLoadedTime,
+--     "N/A" as domLatencyMetricsSample
   ) AS site_speed
+--   ,STRUCT(
+--     "TODO" as exceptionDescription,
+--     -- METRICS
+--     "TODO" as exceptions,
+--     "TODO" as exceptionsPerScreenview,
+--     "TODO" as fatalExceptions,
+--     "TODO" as fatalExceptionsPerScreenview  
+--   ) AS exceptions
   ,STRUCT(
-    "TODO" as exceptionDescription,
-    -- METRICS
-    "TODO" as exceptions,
-    "TODO" as exceptionsPerScreenview,
-    "TODO" as fatalExceptions,
-    "TODO" as fatalExceptionsPerScreenview  
-  ) AS exceptions
-  ,STRUCT(
-    CAST(s0.session_timestamp AS DATE) as date,
-    EXTRACT(YEAR FROM session_timestamp) as year,
-    EXTRACT(MONTH FROM session_timestamp) as month,
-    EXTRACT(WEEK FROM session_timestamp) as week,
-    EXTRACT(DAY FROM session_timestamp) as day,
-    EXTRACT(HOUR FROM session_timestamp) as hour,
-    EXTRACT(MINUTE FROM session_timestamp) as minute,
+    CAST(s0.start_time AS DATE) as date,
+    EXTRACT(YEAR FROM start_time) as year,
+    EXTRACT(MONTH FROM start_time) as month,
+    EXTRACT(WEEK FROM start_time) as week,
+    EXTRACT(DAY FROM start_time) as day,
+    EXTRACT(HOUR FROM start_time) as hour,
+    EXTRACT(MINUTE FROM start_time) as minute,
     "TODO" as nthMonth,
     "TODO" as nthWeek,
     "TODO" as nthDay,
     "TODO" as nthMinute,
-    EXTRACT(DAYOFWEEK FROM session_timestamp) as dayOfWeek,
+    EXTRACT(DAYOFWEEK FROM start_time) as dayOfWeek,
     "TODO" as dayOfWeekName,
     "TODO" as dateHour,
     "TODO" as dateHourMinute,
     "TODO" as yearMonth,
     "TODO" as yearWeek,
-    EXTRACT(ISOWEEK FROM session_timestamp) as isoWeek,
-    EXTRACT(ISOYEAR FROM session_timestamp) as isoYear,
+    EXTRACT(ISOWEEK FROM start_time) as isoWeek,
+    EXTRACT(ISOYEAR FROM start_time) as isoYear,
     "TODO" as isoYearIsoWeek,
     "TODO" as nthHour    
   ) AS time
-  ,STRUCT(
-    "TODO" as appInstallerId,
-    "TODO" as appVersion,
-    "TODO" as appName,
-    "TODO" as appId,
-    "TODO" as screenName,
-    "TODO" as screenDepth,
-    "TODO" as landingScreenName,
-    -- METRICS
-    "TODO" as exitScreenName,
-    "TODO" as screenviews,
-    "TODO" as uniqueScreenviews,
-    "TODO" as screenviewsPerSession,
-    "TODO" as timeOnScreen,
-    "TODO" as avgScreenviewDuration
-  ) AS app_tracking
-  ,STRUCT(
-    "TODO" as pageTitle,
-    "TODO" as landingPageDepth,
-    "TODO" as secondPageDepth,
-    "TODO" as exitPagePath,
-    "TODO" as previousPagePath,
-    "TODO" as pageDepth,
-    -- METRICS
-    "TODO" as pageValue,
-    "TODO" as entrances,
-    "TODO" as entranceRate,
-    "TODO" as pageviews,
-    "TODO" as pageviewsPerSession,
-    "TODO" as uniquePageviews,
-    "TODO" as timeOnPage,
-    "TODO" as avgTimeOnPage,
-    "TODO" as exits,
-    "TODO" as exitRate
-  ) AS page_tracking
-  ,STRUCT(
-    "TODO" as eventCategory,
-    "TODO" as eventAction,
-    "TODO" as eventLabel,
-    -- METRICS
-    "TODO" as totalEvents,
-    "TODO" as uniqueEvents,
-    "TODO" as eventValue,
-    "TODO" as avgEventValue,
-    "TODO" as sessionsWithEvent,
-    "TODO" as eventsPerSessionWithEvent
-  ) AS event_tracking
+--   ,STRUCT(
+--     "N/A" as appInstallerId,
+--     "TODO" as appVersion,
+--     "TODO" as appName,
+--     "TODO" as appId,
+--     "TODO" as screenName,
+--     "TODO" as screenDepth,
+--     "TODO" as landingScreenName,
+--     -- METRICS
+--     "TODO" as exitScreenName,
+--     "TODO" as screenviews,
+--     "TODO" as uniqueScreenviews,
+--     "TODO" as screenviewsPerSession,
+--     "TODO" as timeOnScreen,
+--     "TODO" as avgScreenviewDuration
+--   ) AS app_tracking
+--   ,STRUCT(
+--     "TODO" as pageTitle,
+--     "TODO" as landingPageDepth,
+--     "TODO" as secondPageDepth,
+--     "TODO" as exitPagePath,
+--     "TODO" as previousPagePath,
+--     "TODO" as pageDepth,
+--     -- METRICS
+--     "TODO" as pageValue,
+--     "TODO" as entrances,
+--     "TODO" as entranceRate,
+--     "TODO" as pageviews,
+--     "TODO" as pageviewsPerSession,
+--     "TODO" as uniquePageviews,
+--     "TODO" as timeOnPage,
+--     "TODO" as avgTimeOnPage,
+--     "TODO" as exits,
+--     "TODO" as exitRate
+--   ) AS page_tracking
+--   ,STRUCT(
+--     "TODO" as eventCategory,
+--     "TODO" as eventAction,
+--     "TODO" as eventLabel,
+--     -- METRICS
+--     "TODO" as totalEvents,
+--     "TODO" as uniqueEvents,
+--     "TODO" as eventValue,
+--     "TODO" as avgEventValue,
+--     "TODO" as sessionsWithEvent,
+--     "TODO" as eventsPerSessionWithEvent
+--   ) AS event_tracking
 FROM
   s0,
   sessions,
@@ -217,9 +225,9 @@ FROM
   averages,
   sums
 WHERE TRUE
-  AND s0.d = sessions.d
-  AND s0.d = bounces.d 
-  AND s0.d = averages.d
-  AND s0.d = sums.d
+  AND s0.date = sessions.date
+  AND s0.date = bounces.date 
+  AND s0.date = averages.date
+  AND s0.date = sums.date
 ORDER BY date
 LIMIT 1000
